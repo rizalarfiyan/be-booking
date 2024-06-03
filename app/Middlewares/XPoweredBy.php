20ce51dbbc8c4f93e25ec34971c082155738887f
Muhamad Rizal Arfiyan <rizal.arfiyan.23@gmail.com>
2024-06-03 07:08:23 +0700

    feat: add middlewares
    Signed-off-by: Muhamad Rizal Arfiyan <rizal.arfiyan.23@gmail.com>

---
+++ app/Middlewares/XPoweredBy.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Middlewares;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class XPoweredBy implements MiddlewareInterface
+{
+    /**
+     * @param  ServerRequestInterface  $request
+     * @param  RequestHandlerInterface $handler
+     * @return ResponseInterface
+     */
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $response  = $handler->handle($request);
+
+        return $response->withHeader('X-Powered-By', 'Api Booking Apps');
+    }
+}
